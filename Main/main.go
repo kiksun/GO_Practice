58825b2fe9d9c840d3a10b4ee0d864ec57335400
Mokuson <38876698+kiksun@users.noreply.github.com>
2020-05-13 00:06:43 +0900

    変数、if文の使用

--- Main/main.go
+++ Main/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+func Checknumber(x int)bool{
+	if x==3  {return false 
+	}else {return true}
+}
 func main() {
-	fmt.Println("hello world")
+	msg :="hello world"
+	a,b :=10,15
+	var (
+		c int
+		d string
+	)
+	c=20
+	d="hoge"
+	f:=Checknumber(3)
+	fmt.Println(a,b,c,d,msg)
+	fmt.Println(f)
 }
