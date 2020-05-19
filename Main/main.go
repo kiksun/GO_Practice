53e70daa657fc0a920a418984332f30702aeae8b
Mokuson <38876698+kiksun@users.noreply.github.com>
2020-05-19 18:14:47 +0900

    ginの導入
    main.goを書き換えた

--- Main/main.go
+++ Main/main.go
@@ -1,21 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
+	
 
-func Checknumber(x int)bool{
-	if x==3  {return false 
-	}else {return true}
-}
 func main() {
-	msg :="hello world"
-	a,b :=10,15
-	var (
-		c int
-		d string
-	)
-	c=20
-	d="hoge"
-	f:=Checknumber(3)
-	fmt.Println(a,b,c,d,msg)
-	fmt.Println(f)
+        r := gin.Default()
+        r.GET("/", func(c *gin.Context) {
+                c.String(200, "Hello,World!")
+        })
+        r.Run()
 }
